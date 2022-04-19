816c7b66965498a4b47cb05a36fb0ebf805c405a
Alex8257 <55807934+Alex8257@users.noreply.github.com>
2022-04-19 12:44:40 +0300

    Новая страница авторизации

--- Meetins.Frontend/Pages/AuthorizePage.razor.cs
+++ Meetins.Frontend/Pages/AuthorizePage.razor.cs
@@ -1,6 +1,7 @@
 ﻿using Meetins.Models.Common;
 using Meetins.Models.User.Input;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Components.Rendering;
 using System;
 using System.Collections.Generic;
@@ -12,7 +13,16 @@ namespace Meetins.Frontend.Pages
 {
     public partial class AuthorizePage:ComponentBase
     {
-        private LoginInput _loginData { get; set; }
+
+        private bool _isMainPage { get; set; } = true;
+        private bool _isPasswordPage { get; set; } = false;
+        private bool _isCreatedProfileStart { get; set; } = false;
+        private bool _isCreatedProfileEnd { get; set; } = false;
+
+        private string _cssMainPage => _isMainPage ? "" : "collapse";
+        private string _cssPasswordPage => _isPasswordPage ? "" : "collapse";
+        private string _cssCreatedProfileStart => _isCreatedProfileStart ? "" : "collapse";
+        private string _cssCreatedProfileEnd => _isCreatedProfileEnd ? "" : "collapse";
 
         protected override async Task OnInitializedAsync()
         {
@@ -20,10 +30,45 @@ namespace Meetins.Frontend.Pages
             _cities = await CommonService.GetAllCities();
             StateHasChanged();
         }
+
+        private void OnPagePassword()
+        {
+            _isMainPage = false;
+            _isPasswordPage = true;
+            _isCreatedProfileStart = false;
+            _isCreatedProfileEnd = false;
+
+            StateHasChanged();
+
+        }
+        private void OnMainPage()
+        {
+            _isMainPage = true;
+            _isPasswordPage = false;
+            _isCreatedProfileStart = false;
+            _isCreatedProfileEnd = false;
+
+        }
+        private void OnCreatedProfileStart()
+        {
+            _isMainPage = false;
+            _isPasswordPage = false;
+            _isCreatedProfileStart = true;
+            _isCreatedProfileEnd = false;
+
+        }
+        private void OnCreatedProfileEnd()
+        {
+            _isMainPage = false;
+            _isPasswordPage = false;
+            _isCreatedProfileStart = false;
+            _isCreatedProfileEnd = true;
+
+        }
+        private LoginInput _loginData { get; set; }
         private async Task LoginAsync()
         {
-            await UserService.Login(_loginData); 
-            StateHasChanged();
+            await UserService.Login(_loginData);
             NavigationManager.NavigateTo("/", true);
         }
         private RegisterUserInput _registerData { get; set; } = new RegisterUserInput();
