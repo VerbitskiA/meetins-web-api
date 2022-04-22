51705aaa028a3d5a20bfde04b838f0b926caa4f4
Nicholas <kolya.com.org.ua@gmail.com>
2022-04-22 10:10:08 +0300

    Переименовал тесты на GetUserLockoutStatusAsyncTest_ReturnsTrue и GetUserLockoutStatusAsyncTest_ReturnsFalse

---
+++ Meetins.Tests/User/GetUserLockoutStatusTests.cs
@@ -0,0 +1,71 @@
+﻿using Meetins.Abstractions.Repositories;
+using Meetins.Models.User.Output;
+using Meetins.Services.User;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Threading.Tasks;
+
+namespace Meetins.Tests.User
+{
+    [TestClass]
+    public class GetUserLockoutStatusTests : BaseServiceTest
+    {
+        [TestMethod]
+        public async Task GetUserLockoutStatusAsyncTest_ReturnsTrue()
+        {
+            var guid = Guid.NewGuid();
+
+            //Arrange
+            async Task<LockoutStatusOutput> GetMockedLockoutStatus(Guid guid)
+            {
+                return new LockoutStatusOutput
+                {
+                    LockoutEnabled = true,
+                    LockoutEnd = DateTime.Parse("25.04.2022 9:00", System.Globalization.CultureInfo.CreateSpecificCulture("uk-UA")).ToString()
+                };
+            }
+
+            var mockRepository = new Mock<IUserRepository>();
+            mockRepository.Setup(a => a.GetUserLockoutStatusAsync(guid)).Returns(GetMockedLockoutStatus(guid));
+
+            var service = new UserService(mockRepository.Object, RefreshTokenRepository, PostgreDbContext, MailingService, CommonService);
+
+            //Act
+            var result = service.GetUserLockoutStatusAsync(guid);
+
+            //Assert
+            Assert.IsTrue(result.Result.LockoutEnabled == true && result.Result.LockoutEnd == "25.04.2022 09:00:00");
+        }
+
+        [TestMethod]
+        public async Task GetUserLockoutStatusAsyncTest_ReturnsFalse()
+        {
+            var guid = Guid.NewGuid();
+
+            //Arrange
+            async Task<LockoutStatusOutput> GetMockedLockoutStatus(Guid guid)
+            {
+                return new LockoutStatusOutput
+                {
+                    LockoutEnabled = false,
+                    LockoutEnd = ""
+                };
+            }
+
+            var mockRepository = new Mock<IUserRepository>();
+            mockRepository.Setup(a => a.GetUserLockoutStatusAsync(guid)).Returns(GetMockedLockoutStatus(guid));
+
+            var service = new UserService(mockRepository.Object, RefreshTokenRepository, PostgreDbContext, MailingService, CommonService);
+
+            //Act
+            var result = service.GetUserLockoutStatusAsync(guid);
+
+            //Assert
+            Assert.IsTrue(result.Result.LockoutEnabled == false && result.Result.LockoutEnd == "");
+        }
+    }
+}
+
+
+
